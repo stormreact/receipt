f98d1cbec38855d08d73089af3509e1e3e8f2b50
Michael Angerman <stormasm@gmail.com>
2017-09-11 19:08:14 -0700

    working on receipts test

--- src/reducers/receipts.spec.js
+++ src/reducers/receipts.spec.js
@@ -1,141 +1,85 @@
-import cart from './cart'
+import receipts from './receipts'
+import reducer, * as products from './products'
 
-describe('reducers', () => {
-  describe('cart', () => {
+describe('receipt initial state', () => {
+  describe('receipt', () => {
     const initialState = {
-      addedIds: [],
-      quantityById: {}
+        0: {
+          cart: {
+            addedIds: [
+              2,
+              3
+            ],
+            quantityById: {
+              2: 1,
+              3: 1
+            }
+          },
+          detail: {
+            id: 0,
+            timestamp: 1505173544745,
+            total: 31.55,
+            user: "a@b.edu"
+          }
+        },
+        1: {
+          cart: {
+            addedIds: [
+              2,
+              3
+            ],
+            quantityById: {
+              2: 1,
+              3: 1
+            }
+          },
+          detail: {
+            id: 0,
+            timestamp: 1505173544745,
+            total: 31.55,
+            user: "a@b.edu"
+          }
+        }
     }
-
-    it('should provide the initial state', () => {
-      expect(cart(undefined, {})).toEqual(initialState)
-    })
-
-    it('should handle CHECKOUT_REQUEST action', () => {
-      expect(cart({}, { type: 'CHECKOUT_REQUEST' })).toEqual(initialState)
-    })
-
-    it('should handle CHECKOUT_FAILURE action', () => {
-      expect(cart({}, { type: 'CHECKOUT_FAILURE', cart: 'cart state' })).toEqual('cart state')
-    })
-
-    it('should handle ADD_TO_CART action', () => {
-      expect(cart(initialState, { type: 'ADD_TO_CART', productId: 1 })).toEqual({
-        addedIds: [ 1 ],
-        quantityById: { 1: 1 }
-      })
-    })
-
-    describe('when product is already in cart', () => {
-      it('should handle ADD_TO_CART action 1', () => {
-        const state = {
-          addedIds: [ 1, 2 ],
-          quantityById: { 1: 1, 2: 1 }
-        }
-
-        expect(cart(state, { type: 'ADD_TO_CART', productId: 2 })).toEqual({
-          addedIds: [ 1, 2 ],
-          quantityById: { 1: 1, 2: 2 }
-        })
-      })
-
-      it('should handle ADD_TO_CART action 2', () => {
-        const state = {
-          addedIds: [ 1, 2, 3 ],
-          quantityById: { 1: 1, 2: 2, 3:3 }
-        }
-
-        expect(cart(state, { type: 'ADD_TO_CART', productId: 3 })).toEqual({
-          addedIds: [ 1, 2, 3 ],
-          quantityById: { 1: 1, 2: 2, 3:4 }
-        })
-      })
-
-      it('should handle REMOVE_FROM_CART action 1', () => {
-        const state = {
-          addedIds: [ 1, 2 ],
-          quantityById: { 1: 1, 2: 2 }
-        }
-
-        expect(cart(state, { type: 'REMOVE_FROM_CART', productId: 2 })).toEqual({
-          addedIds: [ 1 ],
-          quantityById: { 1: 1 }
-        })
-      })
-
-      it('should handle REMOVE_FROM_CART action 2', () => {
-        const state = {
-          addedIds: [ 1, 2, 3, 4 ],
-          quantityById: { 1: 1, 2: 2, 3: 2, 4: 5 }
-        }
-
-        expect(cart(state, { type: 'REMOVE_FROM_CART', productId: 1 })).toEqual({
-          addedIds: [ 2, 3, 4 ],
-          quantityById: { 2: 2, 3: 2, 4: 5 }
-        })
-      })
-
-      it('should handle REMOVE_FROM_CART action 3', () => {
-        const state = {
-          addedIds: [ 1, 2, 3, 4 ],
-          quantityById: { 1: 1, 2: 2, 3: 2, 4: 5 }
-        }
-
-        expect(cart(state, { type: 'REMOVE_FROM_CART', productId: 4 })).toEqual({
-          addedIds: [ 1, 2, 3],
-          quantityById: { 1: 1, 2: 2, 3: 2 }
-        })
-      })
-
-    })
   })
 })
 
-import reducer, * as products from './products'
-
 describe('reducers', () => {
-  describe('products', () => {
+  describe('receipts', () => {
     let state
 
-    describe('when products are received', () => {
+    describe('when receipts are received', () => {
 
       beforeEach(() => {
         state = reducer({}, {
-          type: 'RECEIVE_PRODUCTS',
-          products: [
-            {
-              id: 1,
-              title: 'Product 1',
-              inventory: 2
+          type: 'RECEIVE_RECEIPTS',
+
+          0: {
+            cart: {
+              addedIds: [
+                2,
+                3
+              ],
+              quantityById: {
+                2: 1,
+                3: 1
+              }
             },
-            {
-              id: 2,
-              title: 'Product 2',
-              inventory: 1
+            detail: {
+              id: 0,
+              timestamp: 1505173544745,
+              total: 31.55,
+              user: "a@b.edu"
             }
-          ]
+          },
+
+
         })
       })
 
-      it('contains the products from the action', () => {
-        expect(products.getProduct(state, 1)).toEqual({
-          id: 1,
-          title: 'Product 1',
-            inventory: 2
-        })
-        expect(products.getProduct(state, 2)).toEqual({
-          id: 2,
-          title: 'Product 2',
-            inventory: 1
-        })
-      })
-
-      it ('contains no other products', () => {
-        expect(products.getProduct(state, 3)).toEqual(undefined)
-      })
-
-      it('lists all of the products as visible', () => {
-        expect(products.getVisibleProducts(state)).toEqual([
+/*
+      it('lists all of the receipts', () => {
+        expect(receipts.getReceipts(state)).toEqual([
           {
             id: 1,
             title: 'Product 1',
@@ -147,28 +91,7 @@ describe('reducers', () => {
           }
         ])
       })
-
-      describe('when an item is added to the cart', () => {
-
-        beforeEach(() => {
-          state = reducer(state, { type: 'ADD_TO_CART', productId: 1 })
-        })
-
-        it('the inventory is reduced', () => {
-          expect(products.getVisibleProducts(state)).toEqual([
-            {
-              id: 1,
-              title: 'Product 1',
-              inventory: 1
-            }, {
-              id: 2,
-              title: 'Product 2',
-              inventory: 1
-            }
-          ])
-        })
-
-      })
+*/
 
     })
   })
