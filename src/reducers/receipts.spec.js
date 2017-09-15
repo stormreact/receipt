aaec48e9c4528ce193d25a8715678bc9353880d5
Michael Angerman <stormasm@gmail.com>
2017-09-15 12:01:30 -0700

    cc

--- src/reducers/receipts.spec.js
+++ src/reducers/receipts.spec.js
@@ -1,5 +1,5 @@
 import { getReceipt, cartReceipts } from "./receipts";
-import reducer from './receipts'
+import reducer from "./receipts";
 
 describe("receipt initial state", () => {
   describe("receipt", () => {
@@ -29,59 +29,67 @@ describe("receipt initial state", () => {
     });
 
     it("should handle receiving a set of receipts", () => {
-
-        const state = reducer({}, {
-        type: "RECEIVE_RECEIPTS",
-        receipts: [
-          {
-            "cart": {
-              "addedIds": [
-                2,
-                3
-              ],
-              "quantityById": {
-                "2": 1,
-                "3": 1
+      const state = reducer(
+        {},
+        {
+          type: "RECEIVE_RECEIPTS",
+          receipts: [
+            {
+              cart: {
+                addedIds: [2, 3],
+                quantityById: {
+                  "2": 1,
+                  "3": 1
+                }
+              },
+              detail: {
+                id: 1000,
+                timestamp: 1505173544745,
+                total: 131.55,
+                user: "a@b.edu"
               }
             },
-            "detail": {
-              "id": 1000,
-              "timestamp": 1505173544745,
-              "total": 131.55,
-              "user": "a@b.edu"
-            }
-          },
-          {
-            "cart": {
-              "addedIds": [
-                1,
-                4
-              ],
-              "quantityById": {
-                "1": 1,
-                "4": 1
+            {
+              cart: {
+                addedIds: [1, 4],
+                quantityById: {
+                  "1": 1,
+                  "4": 1
+                }
+              },
+              detail: {
+                id: 1001,
+                timestamp: 1505173544745,
+                total: 31.55,
+                user: "a@b.edu"
               }
-            },
-            "detail": {
-              "id": 1001,
-              "timestamp": 1505173544745,
-              "total": 31.55,
-              "user": "a@b.edu"
             }
-          }
-        ]
-      })
+          ]
+        }
+      );
 
       const r1000 = {
-      "cart": {"addedIds": [2, 3], "quantityById": {"2": 1, "3": 1}},
-      "detail": {"id": 1000, "timestamp": 1505173544745, "total": 131.55, "user": "a@b.edu"}}
+        cart: { addedIds: [2, 3], quantityById: { "2": 1, "3": 1 } },
+        detail: {
+          id: 1000,
+          timestamp: 1505173544745,
+          total: 131.55,
+          user: "a@b.edu"
+        }
+      };
 
       const r1001 = {
-      "cart": {"addedIds": [1, 4], "quantityById": {"1": 1, "4": 1}},
-      "detail": {"id": 1001, "timestamp": 1505173544745, "total": 31.55, "user": "a@b.edu"}}
+        cart: { addedIds: [1, 4], quantityById: { "1": 1, "4": 1 } },
+        detail: {
+          id: 1001,
+          timestamp: 1505173544745,
+          total: 31.55,
+          user: "a@b.edu"
+        }
+      };
 
-      expect(getReceipt(state,1000)).toEqual(r1000);
-      expect(getReceipt(state,1001)).toEqual(r1001);
+      expect(getReceipt(state, 1000)).toEqual(r1000);
+      expect(getReceipt(state, 1001)).toEqual(r1001);
     });
   });
 });
