4f66252b0880ebadcc52ead93cd1edb9ec6a794a
Michael Angerman <stormasm@gmail.com>
2017-09-15 12:00:09 -0700

    cc

--- src/reducers/receipts.spec.js
+++ src/reducers/receipts.spec.js
@@ -72,7 +72,6 @@ describe("receipt initial state", () => {
         ]
       })
 
-
       const r1000 = {
       "cart": {"addedIds": [2, 3], "quantityById": {"2": 1, "3": 1}},
       "detail": {"id": 1000, "timestamp": 1505173544745, "total": 131.55, "user": "a@b.edu"}}
@@ -84,71 +83,5 @@ describe("receipt initial state", () => {
       expect(getReceipt(state,1000)).toEqual(r1000);
       expect(getReceipt(state,1001)).toEqual(r1001);
     });
-
-
-    /*
-    it('should handle cartReceipts', () => {
-          const state = {
-              0: {
-                cart: {
-                  addedIds: [
-                    2,
-                    3
-                  ],
-                  quantityById: {
-                    2: 1,
-                    3: 1
-                  }
-                },
-                detail: {
-                  id: 0,
-                  timestamp: 1505173544745,
-                  total: 31.55,
-                  user: "a@b.edu"
-                }
-              },
-              1: {
-                cart: {
-                  addedIds: [
-                    2,
-                    3
-                  ],
-                  quantityById: {
-                    2: 1,
-                    3: 1
-                  }
-                },
-                detail: {
-                  id: 0,
-                  timestamp: 1505173544745,
-                  total: 31.55,
-                  user: "a@b.edu"
-                }
-              }
-          }
-
-          expect(getReceipt(state,0)).toEqual({
-            1: {
-              cart: {
-                addedIds: [
-                  2,
-                  3
-                ],
-                quantityById: {
-                  2: 1,
-                  3: 1
-                }
-              },
-              detail: {
-                id: 0,
-                timestamp: 1505173544745,
-                total: 31.55,
-                user: "a@b.edu"
-              }
-            }
-          })
-    })
-
-*/
   });
 });
