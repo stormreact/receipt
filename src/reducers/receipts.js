74c7f20636eb9ffcc74691e56f2a3756bd976431
Michael Angerman <stormasm@gmail.com>
2017-09-20 10:14:41 -0700

    cc

--- src/reducers/receipts.js
+++ src/reducers/receipts.js
@@ -1,26 +1,26 @@
-import { combineReducers } from 'redux'
-import { getTotal } from './index.js'
+import { combineReducers } from "redux";
+import { getTotal } from "./index.js";
 
-import * as fromCart from './cart'
-import * as fromProducts from './products'
+import * as fromCart from "./cart";
+import * as fromProducts from "./products";
 
 import {
   CHECKOUT_SUCCESS,
   RECEIVE_RECEIPTS,
   RECEIPT_DETAIL
-} from '../constants/ActionTypes'
+} from "../constants/ActionTypes";
 
-const receiptDetail = (action) => {
+const receiptDetail = action => {
   return {
-    cart:action.state.cart,
+    cart: action.state.cart,
     detail: {
       id: action.mycounter,
-      timestamp : Date.now(),
+      timestamp: Date.now(),
       total: getTotal(action.state),
       user: "a@b.edu"
     }
- }
-}
+  };
+};
 
 const byReceiptId = (state = {}, action) => {
   switch (action.type) {
@@ -28,35 +28,35 @@ const byReceiptId = (state = {}, action) => {
       return {
         ...state,
         ...action.receipts.reduce((obj, receipt) => {
-          obj[receipt.detail.id] = receipt
-          return obj
+          obj[receipt.detail.id] = receipt;
+          return obj;
         }, {})
-      }
+      };
     case CHECKOUT_SUCCESS:
       let mycounter = action.mycounter;
       let myreceipt = receiptDetail(action);
-      return { ...state,
-        [mycounter] : myreceipt
-      }
+      return {
+        ...state,
+        [mycounter]: myreceipt
+      };
     default:
-      return state
+      return state;
   }
-}
+};
 
 const visibleReceiptIds = (state = [], action) => {
   switch (action.type) {
     case RECEIVE_RECEIPTS:
-      return action.receipts.map(receipt => receipt.detail.id)
+      return action.receipts.map(receipt => receipt.detail.id);
     case CHECKOUT_SUCCESS:
       let mycounter = action.mycounter;
-      return [ ...state, mycounter ]
+      return [...state, mycounter];
     default:
-      return state
+      return state;
   }
-}
+};
 
 const receiptDetailIds = (state = [], action) => {
-
   if (!action) {
     return state;
   }
@@ -64,35 +64,32 @@ const receiptDetailIds = (state = [], action) => {
   switch (action.type) {
     case RECEIPT_DETAIL:
       let id = action.receiptId;
-      return [id]
+      return [id];
     default:
-      return state
+      return state;
   }
-}
+};
 
 export default combineReducers({
   byReceiptId,
   visibleReceiptIds,
   receiptDetailIds
-})
+});
 
-export const getReceipt = (state, id) =>
-  state.byReceiptId[id]
+export const getReceipt = (state, id) => state.byReceiptId[id];
 
-export const getReceiptDetail = (state, id) =>
-    state.byReceiptId[id].detail
+export const getReceiptDetail = (state, id) => state.byReceiptId[id].detail;
 
 export const getVisibleReceipts = state =>
-    state.visibleReceiptIds.map(id => getReceipt(state, id))
+  state.visibleReceiptIds.map(id => getReceipt(state, id));
 
 export const getVisibleReceiptDetails = state =>
-    state.visibleReceiptIds.map(id => getReceiptDetail(state, id))
+  state.visibleReceiptIds.map(id => getReceiptDetail(state, id));
 
-const getQuantity = (state, id) => fromCart.getQuantity(state, id)
-const getProduct = (state, id) => fromProducts.getProduct(state.products, id)
+const getQuantity = (state, id) => fromCart.getQuantity(state, id);
+const getProduct = (state, id) => fromProducts.getProduct(state.products, id);
 
 export const getCartProductsFromReceipt = state => {
-
   const receiptId = state.receipts.receiptDetailIds[0];
 
   if (!state.receipts.byReceiptId[receiptId]) {
@@ -102,28 +99,28 @@ export const getCartProductsFromReceipt = state => {
   const cart = state.receipts.byReceiptId[receiptId].cart;
 
   const rows = cart.addedIds.map(id => ({
-      ...getProduct(state, id),
-      quantity: getQuantity(cart, id)
-    }))
+    ...getProduct(state, id),
+    quantity: getQuantity(cart, id)
+  }));
 
-/*
+  /*
   This is what the returned data structure looks like
   Leave this here for awhile, and I will remove later
 
-  const rows = [
+  const testrows = [
   {
     id: 100,
-    title: 'John',
-    price: 10.11,
+    title: 'Shoes',
+    price: 40.11,
     quantity: 3
   },
   {
     id: 101,
-    title: 'Jack',
-    price: 11.11,
-    quantity: 10
+    title: 'Boots',
+    price: 71.11,
+    quantity: 2
   }
   ];
-*/
+  */
   return rows;
-}
+};
